5f2d83fe0f1e45a27edd1ee430f97e86ab567900
Juan Pablo Perez Fernandez <blackstar371@outlook.com>
2024-09-27 01:57:55 -0600

    Se agrego el objeto de las rutas

---
+++ client/src/pages/Home/Welcome.js
@@ -0,0 +1,9 @@
+export default function Welcome() {
+    return (
+        <>
+            <div className="container-fluid text-center">
+                <h2>Bienvenido al colegio militarizado</h2>
+            </div>
+        </>
+    )
+}
--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,7 +1,7 @@
 export default function Login() {
     return (
-        <div>
-            <h1>Bienvenido al login</h1>
-        </div>
+        <>
+            <h1>Login</h1>
+        </>
     )
 }
