5698e0ba4f59203edd23c21b606b2dc61962062e
JuanPablo01 <blackstar371@outlook.com>
2024-09-27 02:34:41 -0600

    Se agrego parte del end point

--- server/index.js
+++ server/index.js
@@ -1,2 +1,33 @@
 const express = require('express')
 const mysql = require('mysql')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: 'root',
+    database: 'serviciosEscolares'
+})
+
+app.get('/insertPeriod', (req, res) => {
+    const periodName = req.body.periodName
+    const periodDesc = req.body.periodDesc
+    const periodStatus = req.body.periodStatus
+
+    connection.query('INSERT INTO periodos_inscripcion (nombre_periodo, descripcion, estatus) values (?, ?, ?)',
+        [periodName, periodDesc, periodStatus],
+        (err, result) => {
+            if (err) throw err
+            res.send(result)
+        }
+    )
+})
+
+app.listen(3001, () => {
+    console.log("El puerto se abrio en http://localhost:3001")
+})
