4b86ae3f22cd65ae79d98fed721686829323781f
JuanPablo01 <blackstar371@outlook.com>
2024-09-28 00:06:38 -0600

    Se agrego el primer end point

--- client/src/pages/ServiciosEscolares/inscripcion.js
+++ client/src/pages/ServiciosEscolares/inscripcion.js
@@ -1,7 +1,32 @@
+import { useState } from "react"
 import Button from "react-bootstrap/Button"
 import Form from "react-bootstrap/Form"
 
+import axios, { Axios } from 'axios'
+
 export default function Inscripcion() {
+    const [periodName, setPeriodName] = useState('')
+    const [periodDesc, setPeriodDesc] = useState('')
+    const [periodStatus, setPeriodStatus] = useState(false)
+
+    const data = {
+        periodName: periodName,
+        periodDesc: periodDesc,
+        periodStatus: periodStatus
+    }
+
+    const savePeriod = () => {
+        axios.post('http://localhost:3001/insertPeriod', {
+            data
+        })
+            .then(function (response) {
+                console.log(response)
+            })
+            .catch(function (error) {
+                console.log(error)
+            })
+    }
+
     return (
         <div className="p-2">
             <div className="mb-3">
@@ -10,17 +35,22 @@ export default function Inscripcion() {
             <Form>
                 <Form.Group className="mb-3">
                     <Form.Label>Nombre del periodo</Form.Label>
-                    <Form.Control className="mb-4" type="text" placeholder="Ingresa el nombre del periodo" />
+                    <Form.Control className="mb-4" type="text" placeholder="Ingresa el nombre del periodo" onInput={
+                        (e) => setPeriodName(e.target.value)
+                    } />
                     <Form.Label>Descripción</Form.Label>
-                    <Form.Control className="mb-4" type="text" placeholder="Ingresa la descaripcion" />
+                    <Form.Control className="mb-4" type="text" placeholder="Ingresa la descaripcion" onInput={
+                        (e) => setPeriodDesc(e.target.value)
+                    } />
                     <Form.Label>Estatus del periodo</Form.Label>
                     <Form.Check
                         type="switch"
                         id="estatus-switch"
-                        label="Inactivo / Activo"
+                        label={periodStatus ? "Activo" : "Inactivo"}
+                        onChange={(e) => setPeriodStatus(e.target.checked)}
                     />
                     <div className="container-fluid p-0 my-4">
-                        <Button>Guardar</Button>
+                        <Button onClick={savePeriod}>Guardar</Button>
                     </div>
                 </Form.Group>
             </Form>
--- server/index.js
+++ server/index.js
@@ -14,13 +14,24 @@ const connection = mysql.createConnection({
     database: 'serviciosEscolares'
 })
 
-app.get('/insertPeriod', (req, res) => {
-    const periodName = req.body.periodName
-    const periodDesc = req.body.periodDesc
-    const periodStatus = req.body.periodStatus
+app.post('/insertPeriod', (req, res) => {
+    const periodName = req.body.data.periodName
+    const periodDesc = req.body.data.periodDesc
+    const periodStatus = req.body.data.periodStatus
+
+    console.log(req.body)
 
     connection.query('INSERT INTO periodos_inscripcion (nombre_periodo, descripcion, estatus) values (?, ?, ?)',
         [periodName, periodDesc, periodStatus],
+        (err, result) => {
+            if (err) throw err
+            res.send(result)
+        }
+    )
+})
+
+app.get('/get', (req, res) => {
+    connection.query('SELECT * FROM periodos_inscripcion',
         (err, result) => {
             if (err) throw err
             res.send(result)
