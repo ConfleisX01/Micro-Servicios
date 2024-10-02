852b6264cef4fd7a01c587db37a547ab7596cad8
thetrokers <luisleos90@gmail.com>
2024-10-01 20:44:48 -0600

    todo

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -2,26 +2,28 @@ import React, { useState } from 'react';
 import Axios from "axios";
 import { useNavigate } from "react-router-dom";
 import Swal from 'sweetalert2';
-import 'animate.css';
+import '../Login/login.css';
+
 
 function Login() {
   const [numeroEmpleado, setNumeroEmpleado] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
 
-  const iniciosesion = () => {
+  const iniciosesion = async () => {
     if (numeroEmpleado !== password) {
       Swal.fire({
         icon: 'error',
         title: 'Error',
-        text: 'El número de empleado y la contraseña deben ser iguales.'
+        text: 'Error al ingressar usuario o contraseña'
       });
       return;
     }
 
-    Axios.post("http://localhost:3001/login", {
-      numeroEmpleado: numeroEmpleado
-    }).then((response) => {
+    try {
+      const response = await Axios.post("https://m4xj94bw-3002.usw3.devtunnels.ms//login", {
+        numeroEmpleado: numeroEmpleado
+      });
       const userData = response.data;
       const { idUsuario, nombreAreaEmpleado } = userData;
 
@@ -30,7 +32,7 @@ function Login() {
       Swal.fire({
         icon: 'success',
         title: 'Bienvenido',
-        text: `Bienvenido, ${nombreAreaEmpleado}.`
+        text: `Bienvenido, ${nombreAreaEmpleado}. Su contraseña es: ${numeroEmpleado}.`
       });
 
       switch (nombreAreaEmpleado) {
@@ -40,10 +42,7 @@ function Login() {
         case "Recursos Humanos":
           navigate("/recursos_humanos"); 
           break;
-        case "Alumno":
-          navigate("/alumnos");
-          break;
-        case "Profesor":
+        case "Profesores":
           navigate("/profesores");
           break;
         default:
@@ -53,41 +52,98 @@ function Login() {
             text: 'Rol no conocido.'
           });
       }
-    }).catch((error) => {
+    } catch (error) {
       Swal.fire({
         icon: 'error',
         title: 'Error',
         text: 'Error al iniciar sesión.'
       });
-    });
+    }
+  };
+
+  const iniciosesionAspirante = async () => {
+    try {
+      const response = await Axios.post("https://m4xj94bw-3002.usw3.devtunnels.ms//login-aspirante", {
+        correo: numeroEmpleado
+      });
+      const userData = response.data;
+      const { idUsuario, contrasenia } = userData;
+
+      localStorage.setItem("idUsuario", idUsuario);
+
+      Swal.fire({
+        icon: 'success',
+        title: 'Bienvenido',
+        text: `Bienvenido, su contraseña es: ${contrasenia}.`
+      });
+
+      navigate("/alumnos");
+    } catch (error) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: 'Error al iniciar sesión.'
+      });
+    }
   };
 
   return (
-    <div className="container animate__animated animate__fadeIn">
-      <div className="row justify-content-center">
-        <div className="col-md-4">
-          <div className="card border-dark">
-            <div className="card-header bg-dark text-white">
-              <h3>Inicio de sesión</h3>
-            </div>
-            <div className="card-body">
-              <form>
-                <div className="form-group">
-                  <label>Número de Empleado:</label>
-                  <input type="text" className="form-control" value={numeroEmpleado} onChange={(event) => setNumeroEmpleado(event.target.value)} />
-                </div>
-                <div className="form-group">
-                  <label>Contraseña:</label>
-                  <input type="password" className="form-control" value={password} onChange={(event) => setPassword(event.target.value)} />
-                </div>
-                <button type="button" className="btn btn-dark" onClick={iniciosesion}>Iniciar sesión</button>
-              </form>
-            </div>
-          </div>
+    <div className="login-page">
+      <div className="auth_main">  	
+        <input type="checkbox" id="auth_chk" aria-hidden="true" />
+
+        {/* Personal login */}
+        <div className="auth_personal">
+          <form>
+            <label htmlFor="auth_chk" aria-hidden="true" className="auth_label">Personal</label>
+            <input
+              type="text"
+              name="numeroEmpleado"
+              placeholder="Número de Empleado"
+              maxLength="9"
+              pattern="\d{9}"
+              value={numeroEmpleado}
+              onChange={(event) => setNumeroEmpleado(event.target.value)}
+              className="auth_input"
+              required
+            />
+            <input
+              type="password"
+              name="pswd"
+              placeholder="Contraseña"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+              className="auth_input"
+              required
+            />
+            <button type="button" onClick={iniciosesion} className="auth_button">Iniciar sesión</button>
+          </form>
+        </div>
+
+        {/* Aspirante login */}
+        <div className="auth_aspirante">
+          <form>
+            <label htmlFor="auth_chk" aria-hidden="true" className="auth_label">Aspirante</label>
+            <input
+              type="email"
+              name="correo"
+              placeholder="Correo"
+              className="auth_input"
+              required
+            />
+            <input
+              type="password"
+              name="pswd"
+              placeholder="Contraseña"
+              className="auth_input"
+              required
+            />
+            <button type="button" onClick={iniciosesionAspirante} className="auth_button">Iniciar sesión</button>
+          </form>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Login;
